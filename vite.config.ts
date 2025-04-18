45e97dee7bebf3be277243201784338d8a74391a
pluveto <zijing.zhang@proton.me>
2025-04-18 14:28:11 +0800

    feat(vite.config): add git commit hash and tag to env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { execSync } from 'child_process';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+
+function getGitCommitHash() {
+  try {
+    return execSync('git rev-parse --short HEAD').toString().trim();
+  } catch (e) {
+    return 'UNKNOWN';
+  }
+}
+
+function getGitTag() {
+  try {
+    return execSync('git describe --tags --abbrev=0').toString().trim();
+  } catch (e) {
+    return 'UNKNOWN';
+  }
+}
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -9,5 +26,9 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+  },
+  define: {
+    'import.meta.env.VITE_APP_COMMIT_HASH': JSON.stringify(getGitCommitHash()),
+    'import.meta.env.VITE_APP_TAG': JSON.stringify(getGitTag()),
   },
 });
