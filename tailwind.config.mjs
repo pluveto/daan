00a65e59a3aa3bf75c987863684941cd0d1ff02b
pluveto <zijing.zhang@proton.me>
2025-04-18 14:54:52 +0800

    refactor(tailwind.config.mjs): update publicDir to 'assets'

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  publicDir: 'assets',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // Enable dark mode based on class
   theme: {
