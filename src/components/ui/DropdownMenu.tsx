bc6ed9889d0fd57faac7f7ddca3146c512ab04fb
pluveto <zijing.zhang@proton.me>
2025-04-20 01:19:22 +0800

    fix(ui): Add disabled styles to DropdownMenuSubTrigger

--- src/components/ui/DropdownMenu.tsx
+++ src/components/ui/DropdownMenu.tsx
@@ -207,6 +207,7 @@ function DropdownMenuSubTrigger({
       data-inset={inset}
       className={cn(
         'focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[inset]:pl-8',
+        'data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
         className,
       )}
       {...props}
